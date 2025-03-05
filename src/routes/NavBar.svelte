96984295a6748d8af48fd9a12959527caeece509
Michael Real <michaeldreal15@gmail.com>
2025-03-06 00:16:39 +0800

    feat: add active page underline

--- src/routes/NavBar.svelte
+++ src/routes/NavBar.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import logo from '$lib/icons/logo-white-text.svg';
-    import { onMount } from 'svelte';
+    import { page } from '$app/state';
 
     let lastScrollPosition = 0;
     let showNav = true;
@@ -18,28 +18,38 @@
     }}
 />
 <div
-    class="navbar flex items-center justify-center gap-x-64 bg-ra-black p-4 md:space-x-60 {showNav ? 'show' : 'hide'} "
+    class="navbar flex h-24 items-stretch justify-center gap-x-64 bg-ra-black md:space-x-60 {showNav
+        ? 'show'
+        : 'hide'} "
 >
     <a class="flex items-stretch" href="/">
-        <div class="flex">
+        <div class="flex items-stretch">
             <img src={logo} alt="RA Nolido Logo" class="h-full w-full" />
         </div>
     </a>
 
     <nav class="flex items-stretch">
-        <div class="flex items-stretch justify-center justify-items-center gap-5 align-middle">
-            <div class="navtabs flex items-center px-5">
-                <a href="/"> Home </a>
-            </div>
-            <div class="navtabs flex items-center px-5">
-                <a class="navtabs" href="/projects/"> Projects </a>
-            </div>
-            <div class="navtabs flex items-center px-5">
-                <a class="navtabs" href="/about-us/"> About Us </a>
-            </div>
-            <div class="navtabs flex items-center px-5">
-                <a class="navtabs" href="/contact-us/"> Contact Us </a>
-            </div>
+        <div class="flex gap-3">
+            <a href="/" class="flex">
+                <div class="navtabs flex items-center px-6">
+                    <span class={page.url.pathname === '/' ? 'active' : ''}>Home</span>
+                </div>
+            </a>
+            <a href="/projects/" class="flex">
+                <div class="navtabs flex items-center px-6">
+                    <span class={page.url.pathname === '/projects/' ? 'active' : ''}>Projects</span>
+                </div>
+            </a>
+            <a href="/about-us/" class="flex">
+                <div class="navtabs flex items-center px-6">
+                    <span class={page.url.pathname === '/about-us/' ? 'active' : ''}>About Us</span>
+                </div>
+            </a>
+            <a href="/contact-us/" class="flex">
+                <div class="navtabs flex items-center px-6">
+                    <span class={page.url.pathname === '/contact-us/' ? 'active' : ''}>Contact Us</span>
+                </div>
+            </a>
         </div>
     </nav>
 </div>
@@ -63,7 +73,17 @@
         transform: translateY(-100%);
     }
 
+    .navtabs {
+        margin-bottom: 0;
+        padding-bottom: 0;
+    }
     .navtabs:hover {
         background-color: theme('colors.dark-gray');
+    }
+
+    .active {
+        border-bottom: 1px solid theme('colors.ra-white');
+        padding-top: 3px;
+        padding-bottom: 2px;
     }
 </style>
