81e9778afcb435da6c7d8f9d348e61bdc10cf1d6
Michael Real <michaeldreal15@gmail.com>
2025-03-05 14:16:58 +0800

    feat: add navbar animations

--- src/routes/NavBar.svelte
+++ src/routes/NavBar.svelte
@@ -1,8 +1,25 @@
 <script lang="ts">
     import logo from '$lib/icons/logo-white-text.svg';
+    import { onMount } from 'svelte';
+
+    let lastScrollPosition = 0;
+    let showNav = true;
 </script>
 
-<div class="flex items-center justify-center gap-x-64 bg-[#0A1128] p-4 md:space-x-60">
+<svelte:window
+    on:scroll={() => {
+        var currentScrollposition = window.pageYOffset || document.documentElement.scrollTop; //Get current scroll position
+        if (currentScrollposition > lastScrollPosition) {
+            showNav = false;
+        } else {
+            showNav = true;
+        }
+        lastScrollPosition = currentScrollposition;
+    }}
+/>
+<div
+    class="navbar flex items-center justify-center gap-x-64 bg-[#0A1128] p-4 md:space-x-60 {showNav ? 'show' : 'hide'} "
+>
     <a class="flex items-center px-2" href="/">
         <div class="h-24">
             <img src={logo} alt="RA Nolido Logo" class="h-full w-full" />
@@ -31,5 +48,17 @@
     a {
         color: #fefcfb;
         font-size: 19px;
+    }
+
+    .navbar {
+        transition: transform 150ms linear;
+    }
+
+    .show {
+        transform: translateY(0%);
+    }
+
+    .hide {
+        transform: translateY(-100%);
     }
 </style>
