0c2ea730db2d4ef0907052f7ea94626d752eb86b
Benjeouelle Lazaro <balazaro1@up.edu.ph>
2025-03-25 16:21:29 +0800

    feat: add edited pictures for projects
    - Does not yet include pics for hotel & resorts projects, as well as projects from old website (resto, infra, & religious)

--- src/routes/projects/categories.js
+++ src/routes/projects/categories.js
@@ -6,39 +6,31 @@ export const cats = [
         link: '/projects/bldgs',
         projs: [
             {
-                prj_name: 'mvmc',
+                prj_name: 'MVMC',
                 prj_pics: [
-                    '/src/lib/img/prj-imgs/bldgs/mvmc/IMG_1346.JPG',
-                    '/src/lib/img/prj-imgs/bldgs/mvmc/IMG_1357.JPG',
-                    '/src/lib/img/prj-imgs/bldgs/mvmc/IMG_1358.JPG',
-                    '/src/lib/img/prj-imgs/bldgs/mvmc/IMG_1359.JPG',
+                    '/src/lib/img/prj-imgs/bldgs/mvmc/1.png',
+                    '/src/lib/img/prj-imgs/bldgs/mvmc/2.png',
+                    '/src/lib/img/prj-imgs/bldgs/mvmc/3.png',
+                    '/src/lib/img/prj-imgs/bldgs/mvmc/4.png',
+                    '/src/lib/img/prj-imgs/bldgs/mvmc/5.png',
+                    '/src/lib/img/prj-imgs/bldgs/mvmc/6.png',
+                    '/src/lib/img/prj-imgs/bldgs/mvmc/7.png',
                 ],
             },
+        ],
+    },
+    {
+        page_title: 'industrial',
+        cat: 'Industrial',
+        icon: '/src/lib/icons/cats/industrial.png',
+        link: '/projects/industrial',
+        projs: [
             {
-                prj_name: 'note mvm',
+                prj_name: 'Big Boss',
                 prj_pics: [
-                    '/src/lib/img/prj-imgs/bldgs/mvmc/IMG_1346.JPG',
-                    '/src/lib/img/prj-imgs/bldgs/mvmc/IMG_1357.JPG',
-                    '/src/lib/img/prj-imgs/bldgs/mvmc/IMG_1358.JPG',
-                    '/src/lib/img/prj-imgs/bldgs/mvmc/IMG_1359.JPG',
-                ],
-            },
-            {
-                prj_name: 'test',
-                prj_pics: [
-                    '/src/lib/img/prj-imgs/bldgs/mvmc/IMG_1346.JPG',
-                    '/src/lib/img/prj-imgs/bldgs/mvmc/IMG_1357.JPG',
-                    '/src/lib/img/prj-imgs/bldgs/mvmc/IMG_1358.JPG',
-                    '/src/lib/img/prj-imgs/bldgs/mvmc/IMG_1359.JPG',
-                ],
-            },
-            {
-                prj_name: 'testing',
-                prj_pics: [
-                    '/src/lib/img/prj-imgs/bldgs/mvmc/IMG_1346.JPG',
-                    '/src/lib/img/prj-imgs/bldgs/mvmc/IMG_1357.JPG',
-                    '/src/lib/img/prj-imgs/bldgs/mvmc/IMG_1358.JPG',
-                    '/src/lib/img/prj-imgs/bldgs/mvmc/IMG_1359.JPG',
+                    '/src/lib/img/prj-imgs/industrial/big-boss/1.png',
+                    '/src/lib/img/prj-imgs/industrial/big-boss/2.png',
+                    '/src/lib/img/prj-imgs/industrial/big-boss/3.png',
                 ],
             },
         ],
@@ -48,7 +40,74 @@ export const cats = [
         cat: 'Institutional',
         icon: '/src/lib/icons/cats/inst.png',
         link: '/projects/inst',
-        projs: [{}],
+        projs: [
+            {
+                prj_name: 'Cagayan de Oro College',
+                prj_pics: [
+                    '/src/lib/img/prj-imgs/inst/cdo-college/1.png',
+                    '/src/lib/img/prj-imgs/inst/cdo-college/2.png',
+                    '/src/lib/img/prj-imgs/inst/cdo-college/3.png',
+                    '/src/lib/img/prj-imgs/inst/cdo-college/4.png',
+                    '/src/lib/img/prj-imgs/inst/cdo-college/5.png',
+                ],
+            },
+            {
+                prj_name: 'Southwestern University',
+                prj_pics: [
+                    '/src/lib/img/prj-imgs/inst/swu/1.png',
+                    '/src/lib/img/prj-imgs/inst/swu/2.png',
+                    '/src/lib/img/prj-imgs/inst/swu/3.png',
+                    '/src/lib/img/prj-imgs/inst/swu/4.png',
+                    '/src/lib/img/prj-imgs/inst/swu/5.png',
+                    '/src/lib/img/prj-imgs/inst/swu/6.png',
+                    '/src/lib/img/prj-imgs/inst/swu/7.png',
+                    '/src/lib/img/prj-imgs/inst/swu/8.png',
+                    '/src/lib/img/prj-imgs/inst/swu/9.png',
+                    '/src/lib/img/prj-imgs/inst/swu/10.png',
+                    '/src/lib/img/prj-imgs/inst/swu/11.png',
+                    '/src/lib/img/prj-imgs/inst/swu/12.png',
+                ],
+            },
+            {
+                prj_name: 'University of Iloilo HRM Building',
+                prj_pics: [
+                    '/src/lib/img/prj-imgs/inst/ui-hrm/1.png',
+                    '/src/lib/img/prj-imgs/inst/ui-hrm/2.png',
+                    '/src/lib/img/prj-imgs/inst/ui-hrm/3.png',
+                    '/src/lib/img/prj-imgs/inst/ui-hrm/4.png',
+                    '/src/lib/img/prj-imgs/inst/ui-hrm/5.png',
+                    '/src/lib/img/prj-imgs/inst/ui-hrm/6.png',
+                    '/src/lib/img/prj-imgs/inst/ui-hrm/7.png',
+                    '/src/lib/img/prj-imgs/inst/ui-hrm/8.png',
+                    '/src/lib/img/prj-imgs/inst/ui-hrm/9.png',
+                    '/src/lib/img/prj-imgs/inst/ui-hrm/10.png',
+                    '/src/lib/img/prj-imgs/inst/ui-hrm/11.png',
+                    '/src/lib/img/prj-imgs/inst/ui-hrm/12.png',
+                    '/src/lib/img/prj-imgs/inst/ui-hrm/13.png',
+                    '/src/lib/img/prj-imgs/inst/ui-hrm/14.png',
+                    '/src/lib/img/prj-imgs/inst/ui-hrm/15.png',
+                    '/src/lib/img/prj-imgs/inst/ui-hrm/16.png',
+                ],
+            },
+            {
+                prj_name: 'University of Iloilo Uygonco Building',
+                prj_pics: [
+                    '/src/lib/img/prj-imgs/inst/ui-uy/1.png',
+                    '/src/lib/img/prj-imgs/inst/ui-uy/2.png',
+                    '/src/lib/img/prj-imgs/inst/ui-uy/3.png',
+                    '/src/lib/img/prj-imgs/inst/ui-uy/4.png',
+                ],
+            },
+            {
+                prj_name: 'University of Pangasinan',
+                prj_pics: [
+                    '/src/lib/img/prj-imgs/inst/up/1.png',
+                    '/src/lib/img/prj-imgs/inst/up/2.png',
+                    '/src/lib/img/prj-imgs/inst/up/3.png',
+                    '/src/lib/img/prj-imgs/inst/up/4.png',
+                ],
+            },
+        ],
     },
     {
         page_title: 'hotels',
@@ -82,21 +141,66 @@ export const cats = [
         cat: 'Offices',
         icon: '/src/lib/icons/cats/offices.png',
         link: '/projects/offices',
-        projs: [{}],
+        projs: [
+            {
+                prj_name: 'Smithbell Building',
+                prj_pics: [
+                    '/src/lib/img/prj-imgs/offices/smithbell/1.png',
+                    '/src/lib/img/prj-imgs/offices/smithbell/2.png',
+                    '/src/lib/img/prj-imgs/offices/smithbell/3.png',
+                ],
+            },
+        ],
     },
     {
         page_title: 'property',
         cat: 'Property and Development',
         icon: '/src/lib/icons/cats/property.png',
         link: '/projects/property',
-        projs: [{}],
+        projs: [
+            {
+                prj_name: 'Moldex Dawn',
+                prj_pics: [
+                    '/src/lib/img/prj-imgs/property/moldex/1.png',
+                    '/src/lib/img/prj-imgs/property/moldex/2.png',
+                    '/src/lib/img/prj-imgs/property/moldex/3.png',
+                    '/src/lib/img/prj-imgs/property/moldex/4.png',
+                    '/src/lib/img/prj-imgs/property/moldex/5.png',
+                    '/src/lib/img/prj-imgs/property/moldex/6.png',
+                    '/src/lib/img/prj-imgs/property/moldex/7.png',
+                ],
+            },
+        ],
     },
     {
         page_title: 'residential',
         cat: 'Residential',
         icon: '/src/lib/icons/cats/residential.png',
         link: '/projects/residential',
-        projs: [{}],
+        projs: [
+            {
+                prj_name: 'Boncan Residence',
+                prj_pics: [
+                    '/src/lib/img/prj-imgs/residential/boncan/1.png',
+                    '/src/lib/img/prj-imgs/residential/boncan/2.png',
+                ],
+            },
+
+            {
+                prj_name: 'Romulo Residence',
+                prj_pics: [
+                    '/src/lib/img/prj-imgs/residential/romulo/1.png',
+                    '/src/lib/img/prj-imgs/residential/romulo/2.png',
+                    '/src/lib/img/prj-imgs/residential/romulo/3.png',
+                    '/src/lib/img/prj-imgs/residential/romulo/4.png',
+                    '/src/lib/img/prj-imgs/residential/romulo/5.png',
+                    '/src/lib/img/prj-imgs/residential/romulo/6.png',
+                    '/src/lib/img/prj-imgs/residential/romulo/7.png',
+                    '/src/lib/img/prj-imgs/residential/romulo/8.png',
+                    '/src/lib/img/prj-imgs/residential/romulo/9.png',
+                ],
+            },
+        ],
     },
     {
         page_title: 'resto',
