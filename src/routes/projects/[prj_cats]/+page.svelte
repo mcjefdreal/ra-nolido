cd6e0727731ee4b0cfd580b72e5399f7ac03d44b
Michael Real <michaeldreal15@gmail.com>
2025-03-27 13:50:06 +0800

    fix: adjust thumbnail sizes and hero component

--- src/routes/projects/[prj_cats]/+page.svelte
+++ src/routes/projects/[prj_cats]/+page.svelte
@@ -4,12 +4,11 @@
     const page = data.categ.page_title;
 </script>
 
-<div class='w-full h-10 p-12'>
-    <h1 class='w-full mb-8 text-5xl font-semibold text-ra-black text-center'>{data.categ.cat}</h1>
+<div class="h-10 w-full p-12">
+    <h1 class="mb-8 w-full text-center text-5xl font-semibold text-ra-black">{data.categ.cat}</h1>
 </div>
-<!-- <h2>{page}</h2> -->
 <div class="place-items-center">
-    <div class="my-[75px] mx-[100px] grid grid-cols-3 place-items-center items-stretch gap-5">
+    <div class="my-[75px] grid grid-cols-3 place-items-center items-stretch gap-5">
         <Thumbnails {data}></Thumbnails>
     </div>
 </div>
