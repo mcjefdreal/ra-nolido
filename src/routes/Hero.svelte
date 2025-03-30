cb6579e509e35154301fa1b7a48c9b2f7841eb7c
Benjeouelle Lazaro <balazaro1@up.edu.ph>
2025-03-31 07:52:51 +0800

    feat(Hero.svelte): Remove text & button

--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -21,28 +21,7 @@
     <div class="flex h-full w-full">
         <img src={herobg} alt="background" />
     </div>
-    <div class="absolute top-0 h-full w-full">
-        <div class="flex h-full w-full flex-col items-center justify-between bg-transparent lg:flex-row">
-            <div
-                class="mx-5 flex flex-col items-center gap-8 text-center lg:ml-20 lg:mr-0 lg:items-start lg:text-left xl:ml-40"
-            >
-                <div class="flex flex-col text-ra-white">
-                    <h2 class="text-4xl font-bold leading-tight">
-                        {heading}
-                    </h2>
-                    <h1 class="text-5xl font-bold text-royal-blue">
-                        {subheading}
-                    </h1>
-                </div>
-                <a href="{base}/contact-us">
-                    <button class="optional-btn rounded bg-dark-blue px-4 py-2 font-bold text-white">
-                        {button_content}
-                    </button>
-                </a>
-            </div>
-        </div>
-    </div>
-    <a href="#who_we_are" onclick={handleAnchorClick}>
+    <a href="#three_columns" onclick={handleAnchorClick}>
         <div class="heroscroll content bottom-[10px] h-[30px] w-full">
             <img class="content bottom-0 max-h-[100%] w-auto" src={downArrow} alt="down button" />
         </div>
@@ -64,15 +43,5 @@
         left: 0;
         right: 0;
         margin-inline: auto;
-    }
-    .optional-btn {
-        background: linear-gradient(to left, #0102f2 50%, #fefcfb 50%) right;
-        background-size: 200% 100%;
-        transition: all 0.5s ease;
-    }
-
-    .optional-btn:hover {
-        background-position: left;
-        color: #0a1128;
     }
 </style>
