a12f252e4e16f5b69fb7e47307e1b2fa37697d3e
Marcus Pascual <marcusjoaquim05@gmail.com>
2025-03-30 17:21:06 +0800

    change base path in svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ export default {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.end.NODE_ENV === 'production' ? '/sveltekit-github-pagess' : '',
 		}
 	}
 };
