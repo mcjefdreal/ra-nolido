73574bd710ef99bc523e827d94501602ffba7e65
Marcus Pascual <marcusjoaquim05@gmail.com>
2025-03-30 20:42:04 +0800

    config change

--- svelte.config.js
+++ svelte.config.js
@@ -6,12 +6,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {Config} */
 export default {
     preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
+    kit: {
+        adapter: adapter(),
+        paths: {
+            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+        },
+    },
 };
