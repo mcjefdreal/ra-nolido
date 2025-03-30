626d75b92fd966129598a1e0b68faf598b7725bc
Michael Real <michaeldreal15@gmail.com>
2025-03-30 22:48:25 +0800

    fix: hero image

--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import { base } from '$app/paths';
     import downArrow from '$lib/icons/down_arrow.svg';
-    import herobg from '$lib/img/hero-home/hero-bg.png';
 
     const { heading, subheading, button_content } = $props();
 
@@ -18,7 +17,7 @@
 </script>
 
 <div>
-    <div class="flex h-[100vh] w-full flex-col items-center justify-between bg-[{herobg}] bg-no-repeat lg:flex-row">
+    <div class="bg-hero flex h-[100vh] w-full flex-col items-center justify-between bg-no-repeat lg:flex-row">
         <div
             class="mx-5 flex flex-col items-center gap-8 text-center lg:ml-20 lg:mr-0 lg:items-start lg:text-left xl:ml-40"
         >
