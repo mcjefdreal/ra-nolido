5862bc6bab7fe4c7808a554311920a212be3b59c
Benjeouelle Lazaro <balazaro1@up.edu.ph>
2025-03-31 07:53:29 +0800

    feat(+page.svelte): Remove Who We Are Section & Adjust Three Columns

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,19 +12,8 @@
     <Hero heading={'SA R.A. NOLIDO'} subheading={'SOLIDO'} button_content={'Get in Touch'}></Hero>
 </div>
 
-<div class="ml-24" id="who_we_are">
-    <HighlightText
-        heading={'Who We Are'}
-        body_1={'The company was established by Engr. Rizal Alberto Nolido on May 1969 in Manila as R.A. NOLIDO & COMPANY, INC. primarily to complement the nationwide demand for housing construction and subdivision development. It then ventured into warehouse and superstructure building and ngineering works, reaching its heights during the construction peak of the seventies into the eighties. '}
-        body_2={'In 1991, it was re-incorporated and renamed R.A. NOLIDO CONSTRUCTION CORPORATION (RANCC), with Engr. Ramon Alberto Nolido as President. In 1993, RANCC opened its branch in Cebu City to contribute to the construction requirements of Southern Philippines. Through time, the company has increased its Management and Engineering Staff and has been conducting technical support and skills training seminars on a regular basis. With the advent of information technology, came the computerization of various departments and systems. '}
-        body_3={'The company also operates its own workshop and storehouse on a leased property iin Pugadlawin, Quezon City. At present, the company is ready more than ever to face the challenges of a demanding industry.'}
-        button_content={'Learn More About Us'}
-    ></HighlightText>
-</div>
-
-<div class="my-16">
+<div class="my-16" id='three_columns'>
     <Three_Columns
-        heading={'Why R.A. Nolido?'}
         subheading_1={'Construction'}
         subheading_2={'Project Management'}
         subheading_3="Design"
