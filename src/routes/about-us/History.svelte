bb2a2431874784118407f27dd63f99ef0225deea
Marcus Pascual <marcusjoaquim05@gmail.com>
2025-06-14 01:08:16 +0800

    feat: apply changes to History component

--- src/routes/about-us/History.svelte
+++ src/routes/about-us/History.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import About from './About.svelte';
-    import src from '$lib/icons/temp_logo.svg';
+    import src1 from '$lib/people/team/engr-nolido.jpg';
+    import src2 from '$lib/people/team/dr-nolido.png';
+    import TeamCard from './TeamCard.svelte';
 
     const par1 =
         'The company was established by Engr. Rizal Alberto Nolido on May 1969 in Manila as R.A. NOLIDO & COMPANY, INC. primarily to complement the nationwide demand for housing construction and subdivision development. It then ventured into warehouse and superstructure building and engineering works, reaching its heights during the construction peak of the seventies into the eighties.';
@@ -8,11 +10,29 @@
         'In 1991, it was re-incorporated and renamed R.A. NOLIDO CONSTRUCTION CORPORATION (RANCC), with Engr. Ramon Alberto Nolido as President. In 1993, RANCC opened its branch in Cebu City to contribute to the construction requirements of Southern Philippines. Through time, the company has increased its Management and Engineering Staff and has been conducting technical support and skills training seminars on a regular basis.';
     const par3 =
         'With the advent of information technology, came the computerization of various departments and systems. At present, the company is ready more than ever to face the challenges of a demanding industry.';
+
+    const card1Props = {
+        src: src1,
+        alt: 'Image',
+        name: 'Ramon Alberto B. Nolido',
+        position: '',
+    };
+
+    const card2Props = {
+        src: src2,
+        alt: 'Image',
+        name: 'Rizal Alberto C. Nolido',
+        position: '',
+    };
 </script>
 
 <div class="ml-8 mr-16 flex flex-row justify-evenly">
-    <About heading={'Our History'} body_1={par1} body_2={par2} body_3={par3} />
-    <div class="w-3/8 flex flex-row items-center">
-        <img {src} alt={'History picture'} class="mb-3 w-full items-center overflow-hidden shadow-md" />
+    <div class="w-3/5">
+        <About heading={'Our History'} body_1={par1} body_2={par2} body_3={par3} />
+    </div>
+    <div class="w-2/5 flex flex-row items-center gap-8 text-center">
+        <TeamCard {...card1Props} />
+        <TeamCard {...card2Props} />
+        <!-- <img {src} alt={'History picture'} class="mb-3 w-full items-center overflow-hidden shadow-md" /> -->
     </div>
 </div>
--- src/routes/about-us/Team.svelte
+++ src/routes/about-us/Team.svelte
@@ -1,19 +1,11 @@
 <script lang="ts">
     import TeamCard from './TeamCard.svelte';
-    import src from '$lib/people/team/engr-nolido.jpg';
-    const president = {
-        src: src,
-        alt: 'Image',
-        name: 'Ramon Alberto B. Nolido',
-        position: 'President',
-    };
 </script>
 
 <div class="flex flex-col items-center text-center">
     <h1 class="mb-8 text-5xl font-semibold text-ra-black">Our Team</h1>
 
     <div class="flex w-5/6 flex-row justify-center gap-16 md:w-3/4 lg:w-1/2">
-        <TeamCard {...president} />
         <!--TODO: Add more team cards for more personnel later on-->
     </div>
 </div>
