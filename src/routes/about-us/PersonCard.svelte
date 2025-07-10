52ceb4cde144508f877214f4cf94b646c37b25f2
Marcus Pascual <marcusjoaquim05@gmail.com>
2025-07-11 01:54:13 +0800

    chore: run fmt and build

--- src/routes/about-us/PersonCard.svelte
+++ src/routes/about-us/PersonCard.svelte
@@ -3,8 +3,8 @@
     import src2 from '$lib/people/team/dr-nolido.png';
 </script>
 
-<div class="w-2/5 flex flex-row text-center items-center">
-    <div class="relative flex flex-col w-3/5 place-items-center">
+<div class="flex w-2/5 flex-row items-center text-center">
+    <div class="relative flex w-3/5 flex-col place-items-center">
         <div class="z-10 aspect-square w-full overflow-hidden rounded-full">
             <img src={src1} alt="Ramon Alberto B. Nolido" loading="lazy" class="z-10" />
         </div>
@@ -15,16 +15,16 @@
             </div>
         </div>
 
-        <div class="absolute top-0 left-4/5 flex flex-col w-full place-items-center">
+        <div class="absolute left-4/5 top-0 flex w-full flex-col place-items-center">
             <div class="z-0 aspect-square w-full justify-center overflow-hidden">
                 <img
                     src={src2}
                     alt="Rizal Alberto C. Nolido"
                     loading="lazy"
-                    class="z-0 rounded-xl h-full mx-auto opacity-60"
+                    class="z-0 mx-auto h-full rounded-xl opacity-60"
                 />
                 <!--IMG had class h-full rounded-xl opacity-60 shadow-xl -->
-            </div> 
+            </div>
             <div class="flex w-full flex-row text-center text-sm">
                 <div class="flex w-full flex-col text-center">
                     <h1 class="font-jakarta font-semibold">Rizal Alberto C. Nolido</h1>
