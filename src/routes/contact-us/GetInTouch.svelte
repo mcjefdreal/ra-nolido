7e47192b9296c9945337049f0d7c8995eaaa0130
Michael Real <michaeldreal15@gmail.com>
2025-07-27 15:32:15 +0800

    feat: add map

--- src/routes/contact-us/GetInTouch.svelte
+++ src/routes/contact-us/GetInTouch.svelte
@@ -1,40 +1,64 @@
 <script>
     import facebook from '$lib/icons/facebook.svg';
+    import loc from '$lib/icons/location.svg';
     import mail from '$lib/icons/mail.svg';
     import phone from '$lib/icons/phone.svg';
     import src from '$lib/icons/temp_logo.svg';
 </script>
 
 <div class="flex flex-col items-center py-10">
-    <div class="flex flex-row justify-center gap-8">
+    <div class="flex w-full flex-row justify-center gap-8">
         <div class="flex flex-col justify-evenly">
             <div class="flex flex-row items-center text-left">
-                <a href="mailto:ranolidoconstruction@gmail.com">
-                    <img src={mail} alt="Email" />
-                </a>
-                <h2 class="text-2xl">
-                    <a href="mailto:ranolidoconstruction@gmail.com" class="hover:underline">Send us an email</a>
-                </h2>
-            </div>
-            <div class="flex flex-row items-center text-left">
-                <img src={phone} alt="Phone" />
-                <h2 class="text-left text-2xl">Tel. No. 8352-6419</h2>
-            </div>
-            <div class="flex flex-row items-center text-left">
-                <div>
-                    <a href="https://www.facebook.com/ranolidoconstruction">
-                        <img src={facebook} alt="FB Page" />
-                    </a>
+                <div class="flex w-44 flex-row items-center text-left">
+                    <div>
+                        <a href="https://www.facebook.com/ranolidoconstruction">
+                            <img src={loc} alt="Address" class="rounded-full bg-ra-black p-1" />
+                        </a>
+                    </div>
+                    <h2 class="ml-3 text-2xl">Address</h2>
                 </div>
-                <h2 class="text-2xl">
-                    <a href="https://www.facebook.com/ranolidoconstruction" class="hover:underline"
-                        >Visit our Facebook page</a
-                    >
-                </h2>
+                <div class="ml-5">
+                    <h2 class=" text-left text-xl">165 Roosevelt Ave, Unit B Rama Building</h2>
+                    <h2 class=" text-left text-xl">San Francisco del Monte, Quezon City</h2>
+                    <h2 class=" text-left text-xl">1105 Metro Manila</h2>
+                </div>
+            </div>
+            <div class="flex flex-row items-center text-left">
+                <div class="flex w-44 flex-row items-center text-left">
+                    <img src={phone} alt="Phone" />
+                    <h2 class="ml-2 text-left text-2xl">Phone</h2>
+                </div>
+                <div class="ml-5">
+                    <h2 class="text-left text-xl">+63 997 950 8720 (Viber Ready)</h2>
+                    <h2 class="text-left text-xl">8352-6419 / 3414-1523</h2>
+                </div>
+            </div>
+            <div class="flex flex-row items-center text-left">
+                <div class="flex w-44 flex-row items-center text-left">
+                    <img src={mail} alt="Email" />
+                    <h2 class="ml-2 text-2xl">Email</h2>
+                </div>
+                <div class="ml-5">
+                    <h2 class="text-left text-xl">
+                        <a href="mailto:ranolidoconstruction@gmail.com" class="hover:underline"
+                            >ranolidoconstruction@gmail.com</a
+                        >
+                    </h2>
+                    <h2 class="text-left text-xl">
+                        <a href="mailto:nolidocoprgroup@gmail.com" class="hover:underline">nolidocoprgroup@gmail.com</a>
+                    </h2>
+                </div>
             </div>
         </div>
-        <div class="flex w-1/2 flex-row items-center md:w-1/3 lg:w-1/4">
-            <img {src} alt={'History picture'} class="mb-3 items-center overflow-hidden" />
-        </div>
+        <iframe
+            title="map"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.263469297929!2d121.01374771153755!3d14.640979985791203!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b70e0cf2d6f7%3A0x40b723b63501b50b!2sR.A.%20Nolido%20Construction%20Corporation!5e0!3m2!1sen!2sph!4v1753598633545!5m2!1sen!2sph"
+            width="800"
+            height="500"
+            style="border:0;"
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
     </div>
 </div>
