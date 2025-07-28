dbb3384b9e83a5bf3e652f2ef3280e44346b055d
kalatkarin <carlynecconsebido@gmail.com>
2025-07-28 09:54:33 +0800

    feat(Contact Us): added Let's Talk component
    yay forms

--- src/routes/contact-us/+page.svelte
+++ src/routes/contact-us/+page.svelte
@@ -1,7 +1,12 @@
 <script>
     import GetInTouch from './GetInTouch.svelte';
+    import LetsTalk from './LetsTalk.svelte';
 </script>
 
 <div class="my-16">
     <GetInTouch />
+</div>
+
+<div class="ml-8 my-16 place-items-center">
+    <LetsTalk />
 </div>
